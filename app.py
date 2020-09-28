7412b7711f879bae3d5dc49905acf41b9825b2e3
shakedba03 <shakedba03@gmail.com>
2020-09-28 18:12:39 +0300

    logins

--- app.py
+++ app.py
@@ -15,9 +15,11 @@ time= datetime.datetime.now()
 
 
 is_signed = False
-
+logout = "Log In"
+	
 @app.route('/')
 def home_page():
+	global logout
 	#Uses NASA's API:
 	response = requests.get("https://api.nasa.gov/planetary/apod?api_key=P0sKdiotXiFwGX8J4N3rWli9RFt63IymOHvJF0xT")
 	str_j = json.dumps(response.json())
@@ -32,18 +34,23 @@ def home_page():
 	card_3 = facts_dict[keys_list[2]]
 	card_4 = facts_dict[keys_list[3]]
 	card_5 = facts_dict[keys_list[4]]
+
+	if is_signed:
+		logout = "Log Out"
 	
 	return render_template("index.html", pic_date = pic_date, pic_explanation = pic_explanation, pic_url = pic_url,
-		card_1 = card_1, card_2 = card_2, card_3= card_3, card_4= card_4, card_5 = card_5)
+		card_1 = card_1, card_2 = card_2, card_3= card_3, card_4= card_4, card_5 = card_5, logout=logout)
 
 @app.route('/blog', methods=['GET', 'POST'])
 def blog_page():
+	global logout
 	global is_signed
+	print(is_signed)
 	msg = "- Please log in first."
-	if "username" in login_session:
+	if "username" in login_session and is_signed:
 		msg = " "
 
-	if request.method == 'POST' and "username" in login_session:
+	if request.method == 'POST' and is_signed:
 		name = request.form['name']
 		title = request.form['title']
 		text = request.form['text']
@@ -53,14 +60,16 @@ def blog_page():
 			
 		add_blog(title, picture, name, text, day, month)
 		 
-	return render_template("blog.html", blogs=return_all_blogs(), msg = msg)
+	return render_template("blog.html", blogs=return_all_blogs(), msg = msg, logout=logout)
 
 @app.route('/single_blog/<int:blog_id>')
 def single_blog_page(blog_id):
-	return render_template("single-blog.html", blog = return_blog(blog_id))
+	global logout
+	return render_template("single-blog.html", blog = return_blog(blog_id), logout=logout)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup_page():
+	global logout
 	username_check = "Sign Up"
 	if request.method == 'POST':
 		username = request.form["username"]
@@ -69,15 +78,16 @@ def signup_page():
 		if user != None:
 			if username == user.username:
 				username_check = "Sorry, this username is already taken!"
-				return render_template("signup.html", username_check = username_check)	
+				return render_template("signup.html", username_check = username_check, logout=logout)	
 		add_user(username, password)
 		#return redirect(url_for('login_page'))
-	return render_template("signup.html", username_check = username_check)
+	return render_template("signup.html", username_check = username_check, logout=logout)
 
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
+	global logout
 	global is_signed
 	user_check = "Log In"
 	pass_check = "Password"
@@ -103,24 +113,32 @@ def login_page():
 
 		if username_found and not pass_found:
 			pass_check = "Sorry, this password is incorrect."
-			return render_template("login.html", username = user_check, password = pass_check)
+			return render_template("login.html", username = user_check, password = pass_check, logout=logout)
 
 		elif not username_found and pass_found:
 			user_check = "Sorry, this username is incorrect."
-			return render_template("login.html", username = user_check, password = pass_check)
+			return render_template("login.html", username = user_check, password = pass_check, logout=logout)
 
 		elif not username_found and not pass_found:
 			user_check = "Sorry, this username is incorrect."
 			pass_check = "Sorry, this password is incorrect."
-			return render_template("login.html", username = user_check, password = pass_check)
+			return render_template("login.html", username = user_check, password = pass_check, logout=logout)
 
-	return render_template("login.html", username = user_check, password = pass_check)
-	return render_template("login.html")
+	return render_template("login.html", username = user_check, password = pass_check, logout=logout)
+	#return render_template("login.html")
+
+
+@app.route('/logout')
+def logout_page():
+	global logout
+	login_session.pop("username", None)
+	return redirect(url_for('home_page'))
 
 
 @app.route('/about')
 def about_page():
-	return render_template("about.html")
+	global logout
+	return render_template("about.html", logout=logout)
 
 
 
